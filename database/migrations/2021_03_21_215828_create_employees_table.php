2afc8a0f1bf1245a3a3ffc838acdb30471404136
lookaround3 <darkodrljaca@gmail.com>
2021-03-21 23:14:41 +0100

    Created Employee migration, model and controller

---
+++ database/migrations/2021_03_21_215828_create_employees_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEmployeesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 100);
+            $table->string('address', 250);
+            $table->string('city', 100);
+            $table->string('postal_code', 20);
+            $table->string('country', 100);            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employees');
+    }
+}
