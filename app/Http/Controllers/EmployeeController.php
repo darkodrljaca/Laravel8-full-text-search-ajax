486e34c970f82052678c3410f0247becc2375982
lookaround3 <darkodrljaca@gmail.com>
2021-03-22 00:49:52 +0100

    addedsearchEmployee method

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -3,8 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Employee;
 
 class EmployeeController extends Controller
 {
-    //
+    function index() {
+        return view('employees.index');
+    }
+    
+    function searchEmployee(Request $request) {
+        
+        if($request->ajax()) {
+            $data = Employee::search($request->get('search_text'))->get();
+            return response()->json($data);
+        }
+        
+    }
+    
 }
+
+
