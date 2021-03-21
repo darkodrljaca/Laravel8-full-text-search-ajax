6ace1010a46237eb01330c165b1ae54ce7504225
lookaround3 <darkodrljaca@gmail.com>
2021-03-22 00:55:14 +0100

    Added fields for search

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -4,8 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Nicolaslopezj\Searchable\SearchableTrait;
 
 class Employee extends Model
 {
     use HasFactory;
+    use Notifiable;
+    use SearchableTrait;
+    
+    protected $searchable = [
+        'columns' => [
+            'employees.name'  => 10,
+            'employees.address'   => 10,
+            'employees.city'   => 10,
+            'employees.postal_code'    => 10,
+            'employees.country'  => 10,            
+        ]
+    ];
+        
 }
